8b99fd98fd14ee5d94b4ed0d574978a58840909d
JDKjiang <jieJiang@jiangdeMacBook-Air.local>
2016-12-21 16:21:19 +0800

    16:21

--- js/index.js
+++ js/index.js
@@ -1,44 +1,29 @@
 //轮播图
 $(document).ready(function(){
-	var str = "";
+	
 	$.get("data/index.json",function(data){
-		for(var i=0;i<data.length;i++){
-			str+="<div class='swiper-slide'>"+data[i].img+"</div>"
+		var str = "";
+		for(var i=0;i<data[0]["banner"].length;i++){
+			
+			str+="<div class='swiper-slide'>"+data[0]["banner"][i].img+"</div>"
 			$(".swiper-wrapper").html(str);
+			mySwiper.update();
 		}
+		var arr="";
+		for(var i=0;i<data[0]["list"].length; i++){
+			
+			arr+='<li>'
+						"<a href=''>"+data[0]["list"][i].img+"</a>"
+						"<h5>"+data[0]["list"][i].name+"</h5>"
+						"<p>"+data+[0]["list"][i].price+"</p>"
+				'</li>'
+				$("#m_box1").html(arr);	
+				console.log(data);
+			
+		}
+		
 	})
 })
 
 //产品信息
-$(document).ready(function(){
-	var str = "";
-	$.get("data/index.json",function(data){
-		for(var i=0;i<data.length;i++){
-			str+="<div class='col-xs-6' id='cp_box1'>"
-				"<div id=' cp1'>"+
-					data[i].img1+
-					"<div id='word_box1'>"
-					"<h4>"+data[i].cangs+"</h4>"
-					"<span>"+data[i].pcrice+"</span>"
-					"</div>"
-				"</div>"
-				"<div id='cp2'>"+
-					data[i].img1+
-					"<div id='word_box2'>"
-					"<h4>"+data[i].cangs+"</h4>"
-					"<span>"+data[i].price+"</span>"
-					"</div>"
-				"</div>"
-			"</div>"
-			"<div class='ccol-xs-6' id='cp_box2'>"
-				"<div id ='cp3'>"+
-					data[i].img1+
-					"<div id='word_box3'>"
-					"<h4>"+data[i].cangs+"</h4>"
-					"<span>"+data[i].price+"</span>"
-				"</div>"
-			"</div>"
-			$("＃ploduct_BOX").html(str);
-		}
-	})
-})
+
