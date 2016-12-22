35de144c7d09a34145eff8f13e053e6b13b9df27
17dian <843408503@qq.com>
2016-12-22 15:44:30 +0800

    12
    12

--- js/cart.js
+++
@@ -1,9 +0,0 @@
-var oBtncart = document.getElementById("btn")
-oBtncart.onclick = function () {
-	var oNumcart = document.getElementById("cart-num");
-	var oNumcart2 = document.getElementById("cart-num2");
-	oNumcart.style.display = "none";
-	oNumcart2.style.display = "block";
-    
- 
-}
